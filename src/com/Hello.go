2d9af2756f2205c7755dc211c1605d1c72ff02fe
lswgithub <sowoon123@nate.com>
2016-03-08 17:11:23 +0900

    init golang hello.

--- src/com/Hello.go
+++ src/com/Hello.go
@@ -7,6 +7,31 @@ import (
 )
 
 func main() {
+
 	fmt.Printf("Hello, a GO Lanuage.. \n")
 	fmt.Printf("The time is ", time.Now())
+
+
+	//var age int = 40
+	//fmt.Printf("my age is " , age)
+
+	const pi float64 = 3.14215484215
+	//var isOver40 bool = true
+
+	fmt.Printf("\n %f.3f \n", pi)
+
+
+	/*i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+	*/
+favNum3 := [5] float64{1,2,3,4,5}
+
+	for i, value:=range favNum3 {
+		fmt.Println(i,value)
+	}
+
 }
+
